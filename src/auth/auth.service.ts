960591c9dd7a8abad4822d1c1f9807428d11a173
sulistyp <sulisdeveloper@gmail.,com>
2024-11-20 14:32:48 +0700

    update payload login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,18 +3,22 @@ import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { LoginDto } from './dto/login.dto';
 import * as bcrypt from 'bcrypt';
+import e from 'express';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
-  ) {}
+  ) { }
 
   async login(user: any) {
     const payload = { username: user.username, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
+      username: user.username,
+      id: user,
+      email: user.email,
     };
   }
 
